aac043492fb7b80f9615f5b9e7cdfefa51d2c55d
Stefan Nieuwenburg <nieuwenburg.stefan@gmail.com>
2022-01-04 11:57:42 +0100

    commit

---
+++ main.py
@@ -0,0 +1,19 @@
+__winc_id__ = "cc1b724762854e85a8defa04287f708b"
+__human_name__ = "requests"
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return '<p>Home, sweet home.</p>'
+
+@app.route('/greet/', methods=['GET'])
+def greet():
+    return '<h1>Hello, world!</h1>'
+
+@app.route('/greet/<example_name>', methods=['GET'])
+def greet_user(example_name):
+    return f'<h1>Hello, {example_name}!</h1>'
